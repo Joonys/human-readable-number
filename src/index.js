7422798981ae81917a2c34080587a714c0d57157
Joonys <yurii.kletsko@gmail.com>
2020-02-28 00:03:07 +0200

    complete task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+  const a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const b = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const c = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  transform = (val) => ((val == 0) ? 'zero' : makeHundreds(val));
+
+  makeTens = (val) => ((val < 10) ? a[val] : (val >= 10 && val < 20) ? b[val - 10] : `${c[Math.floor(val / 10)]} ${a[val % 10]}`);
+
+  makeHundreds = (val) => ((val > 99) ? `${a[Math.floor(val / 100)]} hundred ${makeTens(val % 100)}` : makeTens(val));
+
+  return transform(number).trim();
+};
